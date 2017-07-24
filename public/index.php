31ae7f43f42b4b8460068ba09bb8c0514dcd4623
Eduardo Cespedes <lalocespedes@gmail.com>
2017-07-24 00:18:12 -0500

    cambios

--- public/index.php
+++ public/index.php
@@ -7,6 +7,18 @@ require '../vendor/autoload.php';
 
 $app = new \Slim\App;
 
+$app->options('/{routes:.+}', function ($request, $response, $args) {
+    return $response;
+});
+
+$app->add(function ($req, $res, $next) {
+    $response = $next($req, $res);
+    return $response
+            ->withHeader('Access-Control-Allow-Origin', 'https://evening-hamlet-60132.herokuapp.com')
+            ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+});
+
 $app->get('/', function (Request $request, Response $response) {
 
     $response->getBody()->write("Api Start");
